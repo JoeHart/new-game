a36ab02b971e2a9969e1287c2e82c4dff91cfd53
Joe Hart <joehart42@outlook.com>
2019-08-18 08:43:35 +0100

    Basic setup of autorunner

---
+++ new-game-unity/Assets/RunnerCollisionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerCollisionHandler : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    private RunnerControls _runnerControls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _runnerControls = GetComponent<RunnerControls>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+        switch (collision.collider.name)
+        {
+            case "floor":
+                HandleFloorCollision();
+                break;
+        }
+    }
+
+    void HandleFloorCollision()
+    {
+        _runnerControls.TouchFloor();
+    }
+}
---
+++ new-game-unity/Assets/RunnerControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerControls : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    public float jumpSize = 2.0f;
+    private bool _jumping = false;
+    private bool _jumped = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("trigger jump");
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_jumping)
+        {
+            _rigidBody.AddForce(Vector3.up * jumpSize);
+            _jumped = true;
+            _jumping = false;
+        }
+    }
+
+    void Jump()
+    {
+        if (!_jumping && !_jumped)
+        {
+            _jumping = true;
+        }
+    }
+
+    public void TouchFloor()
+    {
+        _jumped = false;
+    }
+
+}

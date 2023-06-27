fdfeade89cfc3ed9bb8b5e8837b53876e8e77f67
Joe Hart <joe@joehart.dev>
2023-06-27 17:51:22 +0100

    update

--- controls/client/index.js
+++ controls/client/index.js
@@ -1,35 +1,43 @@
 import io from "socket.io-client";
 // const socket = io("ws://192.168.0.184:3000");
-const socket = io("ws://10.151.98.19:8080");
-const leftButton = document.getElementById("left");
-const rightButton = document.getElementById("right");
-const fireButton = document.getElementById("fire");
-const handleLeftButton = () => {
-  socket.emit("left");
-};
-const handleRightButton = () => {
-  socket.emit("right");
-};
-const handleFireButton = () => {
-  socket.emit("fire");
-  fireButton.disabled = true;
-  fireButton.innerHTML = "Reloading...";
-  setTimeout(function() {
-    fireButton.disabled = false;
-    fireButton.innerHTML = "Fire";
-  }, 2000);
-};
+try {
 
-socket.on("connect", () => {
-  socket.emit("spawn", { id: socket.id });
-});
 
-socket.on("increment", num => {
-  console.log("increment from server");
-  console.log(display);
-  display.innerHTML = num;
-});
+  const socket = io("ws://localhost:8080");
+  const leftButton = document.getElementById("left");
+  const rightButton = document.getElementById("right");
+  const upButton = document.getElementById("up");
+  const downButton = document.getElementById("down");
 
-leftButton.onclick = handleLeftButton;
-rightButton.onclick = handleRightButton;
-fireButton.onclick = handleFireButton;
+  const handleLeftButton = () => {
+    socket.emit("left");
+  };
+  const handleRightButton = () => {
+    socket.emit("right");
+  };
+  const handleUpButton = () => {
+    socket.emit("up");
+  };
+  const handleDownButton = () => {
+    socket.emit("down");
+  };
+
+
+  socket.on("connect", () => {
+    socket.emit("spawn", { id: socket.id });
+  });
+
+  socket.on("increment", num => {
+    console.log("increment from server");
+    console.log(display);
+    display.innerHTML = num;
+  });
+
+  leftButton.onclick = handleLeftButton;
+  rightButton.onclick = handleRightButton;
+  upButton.onclick = handleUpButton;
+  downButton.onclick = handleDownButton;
+
+} catch (e) {
+  console.log(e);
+}
--- controls/server/index.js
+++ controls/server/index.js
@@ -7,29 +7,37 @@ let number = 0;
 let unitySocket;
 
 io.attach(8080);
-
-io.on("connection", function(socket) {
+console.log("starting server on port 8080");
+io.on("connection", function (socket) {
   console.log("a user connected");
-  socket.on("disconnect", function(data) {
+  socket.on("disconnect", function (data) {
     console.log("user disconnected", socket.id);
     io.to(unitySocket).emit("destroy", { id: socket.id });
   });
   socket.on("spawn", data => {
     io.to(unitySocket).emit("spawn", data);
   });
-  socket.on("left", function() {
+  socket.on("left", function () {
     console.log("LEFT");
     io.to(unitySocket).emit("left", { id: socket.id });
   });
-  socket.on("right", function() {
+  socket.on("right", function () {
     console.log("RIGHT");
     io.to(unitySocket).emit("right", { id: socket.id });
   });
-  socket.on("fire", function() {
+  socket.on("down", function () {
+    console.log("DOWN");
+    io.to(unitySocket).emit("down", { id: socket.id });
+  });
+  socket.on("up", function () {
+    console.log("UP");
+    io.to(unitySocket).emit("up", { id: socket.id });
+  });
+  socket.on("fire", function () {
     console.log("FIRE");
     io.to(unitySocket).emit("fire", { id: socket.id });
   });
-  socket.on("registerGame", function(data) {
+  socket.on("registerGame", function (data) {
     unitySocket = data.id;
   });
   socket.on("spawn", data => {
